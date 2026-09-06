56548ae074a4b9a2ea1c36d42b35eb155afa6ad1
chouaieb amen allah <mariosshopp@gmail.com>
2026-09-07 00:50:14 +0100

    Fix Next.js Hostinger configuration

--- mariosshop/next.config.ts
+++ mariosshop/next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbopack: { root: 'C:\\Users\\SETUP\\OneDrive\\Documents\\marios shop\\mariosshop' },
-
-  /* config options here */
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
